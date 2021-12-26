850bdb36861877ff980d2f61dd7c3f4390d5f66e
Abdelilah EL Haddad <elhaddadabdelilah@gmail.com>
2021-12-26 13:45:20 +0100

    :fire: :art: Commit Reduce Code :sparkles: :globe_with_meridians:

--- page-about.php
+++ page-about.php
@@ -16,22 +16,7 @@
 get_header();
 ?>
 
-<section class="home-slider owl-carousel img"
-  style="background-image: url(<?php if ( has_post_thumbnail() ) the_post_thumbnail_url()?>);">
-
-  <div class="slider-item" style="background-image: url(<?php if ( has_post_thumbnail() ) the_post_thumbnail_url()?>);">
-    <div class="overlay"></div>
-    <div class="container">
-      <div class="row slider-text justify-content-center align-items-center">
-
-        <div class="text-center col-md-7 col-sm-12 ftco-animate">
-          <h1 class="mt-5 mb-3 bread"><?php the_title(); ?></h1>
-        </div>
-
-      </div>
-    </div>
-  </div>
-</section>
+<?php get_template_part('template-parts/content-header'); ?>
 
 <?php get_template_part('template-parts/content-infos'); ?>
 
